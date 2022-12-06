e3f8b55363d98d4098b207c4f43cdff133a924e9
Chris1320 <chris1320is@protonmail.com>
2022-12-06 10:38:41 +0800

    Migrated from `setup.py` to `pyproject.toml` setup format.
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- config_handler/__init__.py
+++ config_handler/__init__.py
@@ -23,3 +23,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+
+from config_handler import info
+
+__version__ = '.'.join(map(str, info.version))
